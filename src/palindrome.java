9ea82c4a2746ce406175bfe49424dff2da38d6dd
Abhishek Kumar <abhi109062@gmail.com>
2021-09-21 14:00:07 +0530

    Done

---
+++ src/palindrome.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class palindrome {
+    int number;
+    palindrome(int number)
+    {
+        this.number=number;
+    }
+    public boolean isPalindrome()
+    {
+        int s=0;
+        int copy=number;
+        while(copy!=0)
+        {
+            int rem=copy%10;
+            s=s*10+rem;
+            copy=copy/10;
+        }
+
+        return s==number;
+    }
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        palindrome ob1=new palindrome(sc.nextInt());
+        System.out.println(ob1.isPalindrome());
+    }
+}
+
